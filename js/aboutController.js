2195c81226a8f623de5813a061bc388cf1f689d8
David Goodwin <goodwid@gmail.com>
2018-02-14 14:25:32 -0800

    mor progress

--- js/aboutController.js
+++
@@ -1,9 +0,0 @@
-(function(module) {
-  var aboutController = {};
-  aboutController.index = function() {
-    document.querySelectorAll('main > section').forEach(el => el.style.display = 'none');
-    document.getElementById('about').style.display = 'block';
-  };
-
-  module.aboutController = aboutController;
-})(window);
--- js/routes.js
+++ js/routes.js
@@ -1,12 +1,33 @@
+/* globals: page */
+
 (function(module) {
   var routes = {};
+  var controllers = {};
+  controllers.selectDisplay = (element) => {
+    document.querySelectorAll('main > section').forEach(el => el.style.display = 'none');
+    document.getElementById(element).style.display = 'block';
+  };
+  controllers.about = () => {
+    controllers.selectDisplay('about');
+  };
+  controllers.contact = () => {
+    controllers.selectDisplay('contact');
+  };
+  controllers.home = () => {
+    controllers.selectDisplay('home');
+  };
+  controllers.location = () => {
+    controllers.selectDisplay('location');
+  };
+
 
   routes.setRouteMappings = function() {
-    page.base('~alicia');
+    page.base('/');
 
-    page('/', homeController.index);
-    page('about', aboutController.index);
-    page('contact', contactController.index);
+    page('/', controllers.home);
+    page('about', controllers.about);
+    page('contact', controllers.contact);
+    page('location', controllers.location);
 
     page();
   };
